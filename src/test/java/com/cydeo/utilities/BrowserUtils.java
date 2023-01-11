6d1abc0eec6a18640596e302663ef422e247e30f
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-01-11 01:26:22 +0100

    BrowserUtils for SwitchWindowAnd Verify

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,35 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
+public class BrowserUtils {
+
+    public static void sleep(int second){
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        }catch (InterruptedException e){
+        }
+
+    }
+
+    public static void switchWindowAndVerify (WebDriver driver, String expectedInUrl, String expectedInTitle ){
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+        }
+
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+}
