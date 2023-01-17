150d42fa5c6f6ada202cfb14c46d0a5b33fa5ef5
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-01-17 22:18:41 +0100

    SingletonPractice practice

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
@@ -1,4 +1,19 @@
 package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
 
 public class Singleton {
+
+    //#1 - create private constructor
+    private Singleton(){     }
+
+    private static String word;
+
+    public static String getWord(){
+        if (word == null){
+            System.out.println("First time call. Word is null");
+        } else {
+            System.out.println("Word already has value");
+        }
+        return word;
+    }
+
 }
