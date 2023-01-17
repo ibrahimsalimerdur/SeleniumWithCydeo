bb3b490c13c38270e9da757483a331120628284e
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-01-17 16:56:42 +0100

    JavaFaker practice

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFaker.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFaker.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
 
+import org.testng.annotations.Test;
+
 public class JavaFaker {
 
-
-
-
+    @Test
+    public void test1(){
+        Fak
+    }
 }
