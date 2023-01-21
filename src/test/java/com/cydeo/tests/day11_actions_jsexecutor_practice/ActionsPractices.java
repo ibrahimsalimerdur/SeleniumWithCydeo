2ad45f426735fe31a77ea15b8e10eb9d51052613
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-01-21 15:48:06 +0100

    day11 task practices

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -1,6 +1,10 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -11,7 +15,15 @@ public class ActionsPractices {
 //2- Go to: https://practice.cydeo.com/
         Driver.getDriver().get("https://practice.cydeo.com/");
 //3- Scroll down to “Powered by CYDEO”
+        Actions actions = new Actions(Driver.getDriver());
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+        actions.moveToElement(cydeoLink).perform();
 //4- Scroll using Actions class “moveTo(element)” method
+
+        actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP).perform();
+        actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP).perform();
+
+
     }
 
 }
---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T7_Scroll_JSExecutor {
+
+    @Test
+    public void task7_scroll_test (){
+//1- Open a chrome browser
+//2- Go to: https://practice.cydeo.com/large
+        Driver.getDriver().get("https://practice.cydeo.com/large");
+//3- Scroll down to “Cydeo” link
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+        WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
+
+        BrowserUtils.sleep(2);
+        js.executeScript("arguments[0].scrollIntoView(true)",cydeoLink);
+
+        BrowserUtils.sleep(2);
+        js.executeScript("arguments[1].scrollIntoView(true)",cydeoLink,homeLink);
+//4- Scroll up to “Home” link
+//5- Use below provided JS method only
+    }
+
+}
